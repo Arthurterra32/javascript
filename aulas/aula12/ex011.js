076180fdfba4dd1982b0928f04193304d1cedeaa
Arthur Terra <117963682+Arthurterra32@users.noreply.github.com>
2022-12-05 22:09:39 -0300

    estudos

---
+++ aulas/aula12/ex011.js
@@ -0,0 +1,13 @@
+var idade = 67
+console.log(`voce tem ${idade} anos.`)
+if (idade < 16) {
+    console.log(`nao vota`)
+}
+else {
+    if (idade < 18 || idade > 65) {//&&=igual a 'e' ||=igual a 'ou'
+        console.log(`voto opcional`)
+    }
+    else {
+        console.log(`vota obrigatorio`)
+    }
+}
